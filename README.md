d5ad3d697be1fff16a0afcaea1cd9006765a16fe
Alicolliar <alicolliar@gmail.com>
2020-03-19 09:58:59 +0000

    Look, shit

--- README.md
+++ README.md
@@ -6,3 +6,12 @@
 If you've ever played the game "Battleships" before, then this is like that, but worse
 
 much, much worse.
+
+## Installation
+Simply copy the code, type `python3 battleships.py`
+
+# Or
+There is no need to install the code if you go to [repl.it](https://text-Battleships.alicolliar.repl.run) and run the program from there.
+
+## How to play
+This should become fairly apparent as the game should prompt you at times on how to play it. If you are unaware of anything concerning battleships, see [here](https://en.wikipedia.org/wiki/Battleship_%28game%29#History).
--- battleships.py
+++ battleships.py
@@ -2,9 +2,11 @@
 #An idea
 import random
 board = ["A1", "A2","A3","A4","A5","B1","B2","B3","B4","B5","C1","C2","C3","C4","C5","D1","D2","D3","D4","D5","E1","E2","E3","E4","E5"]
+player1shippos = []
+player2shippos = []
 
 def bootDisplay(board):
-    print ("\033[2J")   #ANSI Clear Escape Sequence. May not run on all systems.
+    print ("\033[2J")
     print ("\033[u")  
     print("========================================")
     print("========Welcome to Battleships!=========")
@@ -13,6 +15,7 @@ def bootDisplay(board):
     print("",board[0], board[1], board[2], board[3], board[4], "\n", board[5], board[6], board[7], board[8], board[9], "\n", board[10], board[11], board[12], board[13], board[14], "\n", board[15], board[16], board[17], board[18], board[19], "\n", board[20], board[21], board[22], board[23], board[24], "\n\n\n")
 
 def playshipPositionSelect(board):
+    bootDisplay(board)
     x = 10
     playshipos = [] * 10
     for i in range(0,x):
@@ -21,6 +24,7 @@ def playshipPositionSelect(board):
             if inputPos in board:
                 playshipos.append(inputPos)
                 print("Ship added to Board\n")
+                bootDisplay(board)
             else:
                 print("Position not on board\n")
         else:
@@ -40,6 +44,7 @@ def aishipPositionSelect(board):
             x+=1
     return aishipos
 print("\033[s")
+
 bootDisplay(board)
 oneortwo = int(input("Would you like to play with one, or two players?\n> "))
 if oneortwo == 1:
@@ -47,17 +52,17 @@ if oneortwo == 1:
     AI = True
     print("Player 1 please enter your ships starting positions.\n\n")
     player1shippos = playshipPositionSelect(board)
-    print("AI player, please select your ships starting positions.\n\n")
     player2shippos = aishipPositionSelect(board)
 elif oneortwo == 2:
     print("Two player selected, ok")
     AI = False
     print("Player 1 please enter your ships starting positions.\n\n")
     player1shippos = playshipPositionSelect(board)
-    print("Player 2 please enter your ships starting positions. Player 1, look away or something.\n\n")
+    bootDisplay(board)
+    print("Player 2 please enter your ships starting positions.\n\n")
     player2shippos = playshipPositionSelect(board)
 else:
     oneortwo = int(input("Would you like to play with one or two players?(Enter 1 or 2)\n> "))
 
 bootDisplay(board)
-print("Player positions", player1shippos, "\n\nAI positions", player2shippos)
+print("Player positions", player1shippos, "\n\nAI/Player 2 positions", player2shippos)
